b793101894f912aaa50f78b987ad0b11226f3da7
nikosXif <sokinzz@gmail.com>
2017-08-05 00:56:28 +0300

    Update ScanPort.sh

--- ScanPort.sh
+++ ScanPort.sh
@@ -1,1 +1,26 @@
+!/usr/bin/env bash
 
+ip="$1"
+port="$2"
+echo "#######################################"
+echo "######### Welcome to ScanPort ##########"
+echo 
+echo -e "Now checking avalaibility of $ip"
+echo
+ping -c1 $ip >/dev/null 2>&1
+if [ "$?" = 0 ] ;
+then
+        echo "Host seems to be up....OK ;)"
+        echo 
+        echo "Now starting to port scan the IP address: $ip on port ##$port## "
+        echo "========================================================"
+        if [[ "$port" =~ ^[0-9]+$ ]] && [ "$port" -ge 1 -a "$port" -le 65535 ]; then
+                nc -vz $ip $port 
+        else
+                echo "Wrong port please enter port between 1 and 65535: "
+        fi
+else
+        echo "Exiting script....try again ;("
+fi
+
+exit 0;
