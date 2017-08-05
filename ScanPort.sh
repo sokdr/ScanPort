5838f6fa74ca8839598bc5b565c9e8aaf3287f0a
nikosXif <sokinzz@gmail.com>
2017-08-06 00:43:02 +0300

    Update ScanPort.sh

--- ScanPort.sh
+++ ScanPort.sh
@@ -1,26 +1,52 @@
 !/usr/bin/env bash
 
 ip="$1"
-port="$2"
 echo "#######################################"
 echo "######### Welcome to ScanPort ##########"
-echo 
-echo -e "Now checking availability of $ip"
 echo
-ping -c1 $ip >/dev/null 2>&1
-if [ "$?" = 0 ] ;
-then
-        echo "Host seems to be up....OK ;)"
-        echo 
-        echo "Now starting to port scan the IP address: $ip on port ##$port## "
-        echo "========================================================"
-        if [[ "$port" =~ ^[0-9]+$ ]] && [ "$port" -ge 1 -a "$port" -le 65535 ]; then
-                nc -vz $ip $port 
-        else
-                echo "Wrong port please enter port between 1 and 65535: "
-        fi
+ 
+if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then 
+	echo "You entered a valid IPv4 address lets move on:"
+	echo
+	echo -e "Now checking availability of $ip"
+	echo
+	START=$(date +%s)
+	ping -c1 $ip >/dev/null 2>&1
+
+		if [ "$?" = 0 ] ; then
+	        	echo "Host seems to be up....OK ;)"
+	        	echo
+			read -p "Do you want TCP or UDP port scan:// Just press tcp/udp to continue: " answer
+					if [[ $answer = tcp ]] ; then
+					echo "Now starting to scan the IP address: $ip for open TCP ports  "
+	        			echo "========================================================"
+					nc -vz -w 1 -n $ip 1-100
+					echo
+					END=$(date +%s)
+					DIFF=$(( $END - $START ))
+					echo "Script completed in $DIFF seconds :"
+					echo	
+				elif [[ $answer = udp ]]; then 
+					echo "Now starting to scan the IP address: $ip for open UDP ports  "
+	        			echo "========================================================"
+					nc -vz -u -w 1 $ip 20-443
+					echo
+					END=$(date +%s)
+					DIFF=$(( $END - $START ))
+					echo "Script completed in $DIFF seconds :"
+				else 
+					echo "Wrong...Run again and type tcp or udp"
+				fi
+		echo
+	
+		else
+			echo "Host seems to be down....exiting"
+		fi
 else
-        echo "Exiting script....try again ;("
+	echo "Exiting.....Enter a valid IPv4 address: "
+
+
 fi
+
 
 exit 0;
